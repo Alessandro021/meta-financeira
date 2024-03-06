87cee0f7eea7b344765f93d43a4de050c310ee16
alessandro <alessandrodamata6@gmail.com>
2024-03-06 18:53:30 -0300

    feat: adicionado fonts e SplashScreen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { Slot } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 
+import {useFonts, OpenSans_700Bold, OpenSans_400Regular, OpenSans_600SemiBold} from "@expo-google-fonts/open-sans";
 
+SplashScreen.preventAutoHideAsync();
 const Layout = () => {
+
+	const [fontsLoaded] = useFonts({
+		OpenSans_700Bold, 
+		OpenSans_400Regular, 
+		OpenSans_600SemiBold
+	});
+
+	if(fontsLoaded){
+		SplashScreen.hideAsync();
+	} else {
+		return;
+	}
 	return(
 		<>
 			<StatusBar style="dark" />
